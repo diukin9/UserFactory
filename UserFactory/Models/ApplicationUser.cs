e45071e92cbf9a20068ec615a1e3f560607ab7d2
diukin9 <petr.diukin@urfu.me>
2021-07-21 00:57:05 +0500

    Initial commit

---
+++ UserFactory/Models/ApplicationUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace UserFactory.Models
+{
+    public class ApplicationUser : IEquatable<ApplicationUser>
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+
+        public ApplicationUser(NGitLab.Models.User user)
+        {
+            Name = user.Name;
+            Email = user.Email;
+            Username = user.Username;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            try
+            {
+                return this.Username == (obj as ApplicationUser)?.Username;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+            => this.Username == null ? 0 : this.Username.GetHashCode();
+
+        public bool Equals(ApplicationUser other) => this.Username == other.Username;
+    }
+}
---
+++ UserFactory/UserFactory.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Identity;
+using NGitLab;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserFactory.Models;
+
+namespace UserFactory
+{
+    public class UserFactory<T> where T : IdentityUser, new()
+    {
+        private readonly GitLabClient _gitlabClient;
+
+        public UserFactory(string hostUrl, string token)
+        {
+            _gitlabClient = new GitLabClient(hostUrl, token);
+        }
+
+        public async Task AddMissingUsers(List<T> currentUsers, UserManager<T> userManager)
+        {
+            var mails = currentUsers.Select(x => x.Email).ToList();
+            var usernames = currentUsers.Select(x => x.UserName).ToList();
+            var gitlabUsers = this.GetGitLabUsers();
+            foreach (var user in gitlabUsers)
+            {
+                if (!mails.Contains(user.Email) && !usernames.Contains(user.Username))
+                {
+                    await CreateUser(user, userManager);
+                }
+            }
+        }
+
+        protected virtual async Task CreateUser(ApplicationUser missUser, UserManager<T> userManager)
+        {
+            var user = new T()
+            {
+                UserName = missUser.Username,
+                Email = missUser.Email,
+            };
+            var property = typeof(T).GetProperty("Name");
+            if (property != null && property.CanWrite)
+            { 
+                property.SetValue(user, missUser.Name);
+            }
+            await userManager.CreateAsync(user);
+        }
+
+        protected List<ApplicationUser> GetGitLabUsers()
+        {
+            var userQuery = new UserQuery() { IsExternal = true };
+            var externalUsers = _gitlabClient.Users.Get(userQuery)
+                .Select(x => new ApplicationUser(x))
+                .ToList();
+            var users = _gitlabClient.Users.All
+                .Where(user => !user.Name.EndsWith(" Bot") && user.State != "blocked")
+                .Select(x => new ApplicationUser(x))
+                .ToList();
+            users.RemoveAll(externalUsers.Contains);
+            return users;
+        }
+    }
+}
